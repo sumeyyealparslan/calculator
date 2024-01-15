1636cf549f6852e04f1715861751b179e5f641bc
sumeyyealparslan <sumalparslann@gmail.com>
2024-01-15 23:04:10 +0300

    Added addition method for doubles

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,12 @@
-public double addition(double a, double b) {
-return b + a; 
-} 
- 
+public class Calculator{
 
+public int addition(int a, int b) {
+
+return a+ b;
+}
+public double addition(double a, double b) {
+return a + b; 
+}  
+
+}
 
