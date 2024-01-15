84625ad414e456921c6470a71da3b13dac85786d
sumeyyealparslan <sumalparslann@gmail.com>
2024-01-15 23:22:34 +0300

    Added subtraction method

--- Calculator.java
+++ Calculator.java
@@ -4,9 +4,20 @@ public int addition(int a, int b) {
 
 return a+ b;
 }
+
 public double addition(double a, double b) {
 return a + b; 
 }  
+
+public int subtraction(int a, int b) {
+
+return a - b;
+}
+
+public double subtraction(double a, double b) {
+return a - b;
+}
+
 
 }
 
