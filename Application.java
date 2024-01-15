bcffc64c5c48a3a98301f73a057e61e0362ac1bb
sumeyyealparslan <sumalparslann@gmail.com>
2024-01-16 00:50:04 +0300

    Added Application to call the Calculator

---
+++ Application.java
@@ -0,0 +1,6 @@
+public class Application {
+public static void main(String[] args) {
+Calculator calculator = new Calculator();
+System.out.println(calculator.addition(1, 2));
+System.out.println(calculator.addition(1.5, 3.2));}
+}
